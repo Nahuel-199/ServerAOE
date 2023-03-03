402d635ed0613dead60d14f526209f9c7588050c
Nahuel Cernadas <gabrielnahuel96@gmail.com>
2023-03-03 17:12:31 -0300

    changesmongo

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
 mongoose
-.connect(process.env.MONGO_URL)
+.connect("mongodb+srv://Nahuel:44850508@node0.jgo4ibq.mongodb.net/?retryWrites=true&w=majority")
 .then(() => console.log("DB connection Successfull!"))
 .catch((err) => console.log(err));
 }
